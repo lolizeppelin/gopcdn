9d086a4bbb6d4b5b0664318d7f4daf6d75d26602
lolizeppelin <lolizeppelin@gmail.com>
2018-09-21 17:36:07 +0800

    统一fileinfo校验

--- gopcdn/common.py
+++ gopcdn/common.py
@@ -1,3 +1,5 @@
+from goperation.common import FILEINFOSCHEMA
+
 CDN = 'gopcdn'
 
 ENABLE = 1
@@ -6,23 +8,6 @@ DISENABLE = 0
 CACHESETNAME = 'gopcdn-resource-caches'
 CACHETIME = 600
 
-
 DOMAIN = {'type': 'string', 'format': 'hostname'}
 
-
 DOMAINS = {'type': 'array', 'items': DOMAIN, 'description': 'domain hostname list'}
-
-PATHPATTERN = '^[a-z0-9]+?(?!.*?/[\.]{1,}/)([a-z0-9\.\-_/])+?[a-z0-9]+?$'
-
-FILEINFOSCHEMA = {
-    'type': 'object',
-    'required': ['md5', 'size', 'filename'],
-    'properties': {
-        "size": {'type': 'integer', 'minimum': 30},
-        'md5': {'type': 'string', 'format': 'md5'},
-        "ext": {'type': 'string'},
-        "filename": {'type': 'string', "pattern": PATHPATTERN},
-        "overwrite": {'oneOf': [{'type': 'null'},
-                                {'type': 'string', "pattern": PATHPATTERN}]}
-    }
-}
